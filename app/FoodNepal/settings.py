ce5a401eb33efc8967c376b6858e3924089c694a
MdJiyaulHaq <mdjiyaulhaq78692@gmail.com>
2025-07-29 21:31:12 +0545

    update database settings for production database

--- app/FoodNepal/settings.py
+++ app/FoodNepal/settings.py
@@ -79,16 +79,19 @@ WSGI_APPLICATION = "FoodNepal.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("DB_NAME", "mydatabase"),
-        "USER": os.environ.get("DB_USER", "myuser"),
-        "PASSWORD": os.environ.get("DB_PASSWORD", "mypassword"),
-        "HOST": os.environ.get("DB_HOST", "db"),
-        "PORT": os.environ.get("DB_PORT", "5432"),
+if DEBUG:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.environ.get("DB_NAME", "mydatabase"),
+            "USER": os.environ.get("DB_USER", "myuser"),
+            "PASSWORD": os.environ.get("DB_PASSWORD", "mypassword"),
+            "HOST": os.environ.get("DB_HOST", "db"),
+            "PORT": os.environ.get("DB_PORT", "5432"),
+        }
     }
-}
+else:
+    DATABASES = os.getenv("DATABASE_URL")
 
 
 # Password validation
